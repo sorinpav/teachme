a7c6e005db324132bdfa48690bfe8517a0b41648
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-07-25 16:51:08 +0100

    made some styling changes on the signup form, next step is to implement UI for signup methods as a separate component for versatility

--- src/components/Forms/SignupForm/SignupForm.js
+++ src/components/Forms/SignupForm/SignupForm.js
@@ -6,8 +6,30 @@ const SignupForm = () => {
     <div className='SignupForm'>
       <div className='center-container'>
         <div className='grid-container'>
-          <div className='left-side'>Left side</div>
-          <div className='right-side'>Right side</div>
+          <div className='left-side'>
+            <div className='sign-up-intro'>
+              <span className='sign-up-to-teach-or-learn'>
+                Sign up to teach or learn a new skill
+              </span>
+              <span className='make-money-online'>
+                Make money by showcasing your skills and teaching people
+              </span>
+              <span className='copyright'>
+                Made with love by Sorin Pavelescu
+              </span>
+            </div>
+          </div>
+          <div className='right-side'>
+            <div className='sign-up-form'>
+              <div className='get-started'>
+                <span className='get-started-text'>Get Started</span>
+                <span className='login-methods'></span>
+                <span className='already-registered'>
+                  Already have an account? <a href='#'>Login</a>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
